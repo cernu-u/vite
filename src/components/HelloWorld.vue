b52741eb8e757c8f990330c66d7e32c206bc4f92
cernuu <99365885+cernu-u@users.noreply.github.com>
2022-02-11 13:07:30 +0100

    init tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,8 +6,10 @@
       Vite Documentation
     </a>
     |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docu   mentation eeeeee</a>
   </p>
+
+    <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
 
   <button type="button" @click="state.count++">count is: {{ state.count }}</button>
   <p>
